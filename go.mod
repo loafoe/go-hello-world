a89fa6194fafccd52b9afb03d327be403c85a06f
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2023-02-22 10:53:14 +0100

    Implement OpenTelemetry tracing

--- go.mod
+++ go.mod
@@ -6,15 +6,22 @@ require (
 	github.com/labstack/echo-contrib v0.13.1
 	github.com/labstack/echo/v4 v4.10.0
 	go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho v0.38.0
+	go.opentelemetry.io/otel v1.13.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.13.0
+	go.opentelemetry.io/otel/sdk v1.13.0
+	go.opentelemetry.io/otel/trace v1.13.0
+	google.golang.org/grpc v1.53.0
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
@@ -25,12 +32,14 @@ require (
 	github.com/prometheus/procfs v0.8.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	go.opentelemetry.io/otel v1.12.0 // indirect
-	go.opentelemetry.io/otel/trace v1.12.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.13.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.13.0 // indirect
+	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
 	golang.org/x/crypto v0.2.0 // indirect
-	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	golang.org/x/time v0.2.0 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/genproto v0.0.0-20230221151758-ace64dc21148 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"net/http"
@@ -10,13 +11,90 @@ import (
 	"strconv"
 	"time"
 
+	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
 	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
+// Initializes an OTLP exporter, and configures the corresponding trace and
+// metric providers.
+func initProvider() (func(context.Context) error, error) {
+	ctx := context.Background()
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			// the service name used to display traces in backends
+			semconv.ServiceName("go-hello-world"),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create resource: %w", err)
+	}
+
+	// If the OpenTelemetry Collector is running on a local cluster (minikube or
+	// microk8s), it should be accessible through the NodePort service at the
+	// `localhost:30080` endpoint. Otherwise, replace `localhost` with the
+	// endpoint of your cluster. If you run the app inside k8s, then you can
+	// probably connect directly to the service through dns.
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, os.Getenv("OTLP_ADDRESS"),
+		// Note the use of insecure transport here. TLS is recommended in production.
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
+	}
+
+	// Set up a trace exporter
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+	}
+
+	// Register the trace exporter with a TracerProvider, using a batch
+	// span processor to aggregate spans before export.
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	// set global propagator to tracecontext (the default is no-op).
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	// Shutdown will flush any remaining spans and shut down the exporter.
+	return tracerProvider.Shutdown, nil
+}
+
 func main() {
+	//ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	//defer cancel()
+	ctx := context.Background()
+
+	shutdown, err := initProvider()
+	if err == nil {
+		defer func() {
+			if err := shutdown(ctx); err != nil {
+				fmt.Printf("failed to shutdown TracerProvider: %v\n", err)
+			}
+		}()
+	}
+
 	listenString := ":8080"
 
 	// Echo instance
@@ -29,6 +107,8 @@ func main() {
 	if color := os.Getenv("COLOR"); color != "" {
 		instanceIndex = color
 	}
+	tracer := otel.Tracer(fmt.Sprintf("go-hell-world-%s", instanceIndex))
+	ctx = context.Background()
 
 	e.Use(otelecho.Middleware("go-hello-world"))
 	// Middleware
@@ -36,11 +116,10 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.Use()
-	e.GET("/", hello(instanceIndex))
-	e.GET("/api/test/:host/:port", connectTester())
-	e.Any("/dump", requestDumper())
-	e.Any("/build", infoDumper())
+	e.GET("/", hello(ctx, tracer, instanceIndex))
+	e.GET("/api/test/:host/:port", connectTester(ctx, tracer))
+	e.Any("/dump", requestDumper(ctx, tracer))
+	e.Any("/build", infoDumper(ctx, tracer))
 
 	// Metrics
 	ps := echo.New()
@@ -73,26 +152,35 @@ type connectResult struct {
 }
 
 // Handler
-func hello(instanceIndex string) echo.HandlerFunc {
+func hello(ctx context.Context, tracer trace.Tracer, instanceIndex string) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		_, span := tracer.Start(ctx, "hello")
+		defer span.End()
 		return c.String(http.StatusOK, fmt.Sprintf("Hello from instance \"%s\"! You've requested: %s\n", instanceIndex, c.Request().RequestURI))
 	}
 }
 
-func infoDumper() echo.HandlerFunc {
+func infoDumper(ctx context.Context, tracer trace.Tracer) echo.HandlerFunc {
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
 		return func(c echo.Context) error {
+			_, span := tracer.Start(ctx, "info-dumper")
+			defer span.End()
 			return c.String(http.StatusInternalServerError, "build info not available")
 		}
 	}
 	return func(c echo.Context) error {
+		_, span := tracer.Start(ctx, "info-dumper")
+		defer span.End()
 		return c.String(http.StatusOK, info.String())
 	}
 }
 
-func requestDumper() echo.HandlerFunc {
+func requestDumper(ctx context.Context, tracer trace.Tracer) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		_, span := tracer.Start(ctx, "request-dumper")
+		defer span.End()
+
 		pause := 0
 		if wait := c.QueryParam("wait"); wait != "" {
 			val, err := strconv.Atoi(wait)
@@ -113,8 +201,10 @@ func requestDumper() echo.HandlerFunc {
 	}
 }
 
-func connectTester() echo.HandlerFunc {
+func connectTester(ctx context.Context, tracer trace.Tracer) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		_, span := tracer.Start(ctx, "connect-tester")
+		defer span.End()
 		host := c.Param("host")
 		port := c.Param("port")
 		results := rawConnect(host, []string{port})
